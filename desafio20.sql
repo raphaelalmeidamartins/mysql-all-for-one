bb44b01bdb712997a929082ae4938ef00d34d28d
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-25 17:42:33 -0300

    :sparkles: desafio 20 concluído

---
+++ desafio20.sql
@@ -0,0 +1,22 @@
+INSERT INTO northwind.order_details (
+	order_id,
+  product_id,
+  quantity,
+  unit_price,
+  discount,
+  status_id,
+  date_allocated,
+  purchase_order_id,
+  inventory_id
+)
+VALUES (
+	69,
+  80,
+  15.0000,
+  15.0000,
+  0,
+  2,
+  NULL,
+  NULL,
+  129
+);
